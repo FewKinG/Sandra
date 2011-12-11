01c97a3c5be30225ab334eeb8199c4d06647101a
Sven C <fewking@paniccrew.de>
2011-12-11 21:20:02 +0100

    replaced && with and

--- lib/sandra/key_validator.rb
+++ lib/sandra/key_validator.rb
@@ -4,7 +4,7 @@ module Sandra
       if record.attributes[attribute.to_s].nil?
         record.errors[attribute] << "#{attribute.to_s} cannot be nil."
       elsif not record.class.super_column_name
-        record.errors[attribute] << "#{record.send(attribute.to_s)} has been taken." if record.new_record? && record.class.get(record.attributes[attribute.to_s])
+        record.errors[attribute] << "#{record.send(attribute.to_s)} has been taken." if record.new_record? and record.class.get(record.attributes[attribute.to_s])
       end
     end
   end
